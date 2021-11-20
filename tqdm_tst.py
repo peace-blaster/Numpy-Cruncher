a1b03e8315bbdc6d5213dae845c9b852732046b9
peaceblaster <53p3n7@gmail.com>
2021-11-20 14:02:09 -0800

    started DEV branch, working on adjustable test intesity

--- tqdm_tst.py
+++ tqdm_tst.py
@@ -1,21 +1,25 @@
 from tqdm import tqdm
 from multiprocessing.pool import ThreadPool
 import numpy
+from functools import partial
 
 def cubes(bla):
     return sum(sum(sum(numpy.random.rand(1000,1000,1000))))
 
 def IOLoop():
     try:
-        calc(int(input('\nInput number of iterations to test: ')), int(input('\nInput number of threads for Pool: ')))
+        calc(int(input('\nSelect test intensity (0 to 10): '))
+            , int(input('\nInput number of iterations to test: '))
+            , int(input('\nInput number of threads for Pool: ')))
     except:
         print('\nInvalid input, try again.\n')
         IOLoop()
 
-def calc(tst_len, threads):
+def calc(intensity, tst_len, threads):
     print('\n')
+    func = partial(cubes, intensity)
     for _ in tqdm(
-        ThreadPool(threads).imap_unordered(cubes, range(tst_len)),
+        ThreadPool(threads).imap_unordered(func, range(tst_len)),
         total=tst_len,
         desc=f"Processing...",
     ):
